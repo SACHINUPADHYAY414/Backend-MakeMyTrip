d5a6d39963dcbc064cb453b6f66b5639524e9bb8
Sachin Kumar Upadhyay <137228483+SACHINUPADHYAY414@users.noreply.github.com>
2025-08-01 21:15:24 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,37 +1,62 @@
-const db = require('../config/db');
+const db = require("../config/db");
 
 const createUser = async (userData) => {
   const {
-    title, first_name, last_name, gender, date_of_birth,
-    present_address_line1, present_pincode, present_country,
-    present_state, present_city, email, password_hash, salt
+    title,
+    first_name,
+    last_name,
+    gender,
+    date_of_birth,
+    present_address_line1,
+    present_pincode,
+    present_country,
+    present_state,
+    present_city,
+    email,
+    mobileNumber,
+    password_hash,
+    salt
   } = userData;
 
-  const result = await db.query(`
+  const result = await db.query(
+    `
     INSERT INTO users (
-      title, first_name, last_name, gender, date_of_birth,
-      present_address_line1, present_pincode, present_country,
-      present_state, present_city, email, password_hash, salt
-    ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)
+  title, first_name, last_name, gender, date_of_birth,
+  present_address_line1, present_pincode, present_country,
+  present_state, present_city, email, mobile_number, password_hash, salt
+)
+ VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)
     RETURNING id
-  `, [
-    title, first_name, last_name, gender, date_of_birth,
-    present_address_line1, present_pincode, present_country,
-    present_state, present_city, email, password_hash, salt
-  ]);
+  `,
+    [
+      title,
+      first_name,
+      last_name,
+      gender,
+      date_of_birth,
+      present_address_line1,
+      present_pincode,
+      present_country,
+      present_state,
+      present_city,
+      email,
+      mobileNumber,
+      password_hash,
+      salt
+    ]
+  );
 
   return result.rows[0];
 };
 
 const findUserByEmail = async (email) => {
-  const result = await db.query(
-    'SELECT * FROM users WHERE email = $1',
-    [email]
-  );
+  const result = await db.query("SELECT * FROM users WHERE email = $1", [
+    email
+  ]);
   return result.rows[0];
 };
 
 module.exports = {
   createUser,
-  findUserByEmail,
+  findUserByEmail
 };
