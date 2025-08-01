be64cb01d0c130c31073be2e2a48920ba8dfa297
Sachin Kumar Upadhyay <137228483+SACHINUPADHYAY414@users.noreply.github.com>
2025-08-01 21:45:59 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,3 +1,17 @@
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
+// module.exports = pool;
+
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
@@ -7,6 +21,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
