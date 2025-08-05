8755c31b727bc45696ef29e7fc54526282f3101b
SACHINUPADHYAY414 <upadhyaysachin414@gmail.com>
2025-08-05 19:02:27 +0530

    fresh updated code

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// require('dotenv').config();
-
-// const app = express();
-
-// // app.use(cors({
-// //   origin: 'http://localhost:3000',
-// //   credentials: true,
-// // }));
-// app.use(cors({
-//   origin: "*"
-// }));
-
-// app.use(express.json());
-
-// const apiRoutes = require('./routes/apiRoutes');
-// app.use('/api', apiRoutes);
-
-// module.exports = app;
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
