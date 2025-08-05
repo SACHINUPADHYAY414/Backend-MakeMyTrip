f4504823e295e569e1b5ea8f57b268b4bfba3859
Sachin Kumar Upadhyay <137228483+SACHINUPADHYAY414@users.noreply.github.com>
2025-08-05 18:41:30 +0530

    Update bookingModel.js

--- models/bookingModel.js
+++ models/bookingModel.js
@@ -1,5 +1,4 @@
 const db = require('../config/db');
-
 const createBooking = async (bookingData) => {
   const {
     busId,
@@ -18,8 +17,8 @@ const createBooking = async (bookingData) => {
 
   const result = await db.query(
     `INSERT INTO booking_table (
-      busId, seatId, firstName, lastName, email, mobileNumber,
-      presentAddressLine1, dateOfBirth, coupon, couponDiscount, totalPrice, userId
+      busid, seatid, firstname, lastname, email, mobilenumber,
+      presentaddressline1, dateofbirth, coupon, coupondiscount, totalprice, userid
     ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)
     RETURNING *`,
     [
@@ -38,8 +37,9 @@ const createBooking = async (bookingData) => {
     ]
   );
 
-  return result.rows[0];
+  return result.rows[0]; // returns booking ID and data
 };
+
 
 const getAllBookings = async () => {
   const result = await db.query('SELECT * FROM booking_table');
@@ -47,9 +47,15 @@ const getAllBookings = async () => {
 };
 
 const getBookingById = async (id) => {
-  const result = await db.query('SELECT * FROM booking_table WHERE id = $1', [id]);
+  const result = await db.query(`
+    SELECT b.*, bus.from_city_name, bus.to_city_name
+    FROM booking_table b
+    JOIN bus_table bus ON b.busid = bus.id
+    WHERE b.id = $1
+  `, [id]);
   return result.rows[0];
 };
+
 
 module.exports = {
   createBooking,
